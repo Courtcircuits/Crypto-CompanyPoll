0d7cc6b6125209c4c176623adc34b1fbea7a5a40
Radulescu Tristan-Mihai <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-01-14 16:37:45 +0100

    décrypt mais fonctionne pas

--- Scrutateur/src/main/java/CryptoUtils.java
+++ Scrutateur/src/main/java/CryptoUtils.java
@@ -1,9 +1,13 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CryptoUtils {
+
+    private static List<KeyHolder> holders = new ArrayList<>();
 
     private static BigInteger polynome(BigInteger[] coefs, BigInteger x){
         BigInteger result = BigInteger.ZERO;
@@ -69,25 +73,79 @@ public class CryptoUtils {
             BigInteger x;
             do{
                 x = new BigInteger(bit, new Random());
-            }while (x.compareTo(high)>=0);
-            shares[i] = polynome(coefs, x);
-            System.out.println(shares[i]);
+            }while (x.compareTo(high)>0);
+
+            holders.add(new KeyHolder(x, polynome(coefs, x)));
+
         }
 
         return new Key[]{new PublicKey(p, g, b), new SecretKey(a)};
     }
 
-    public static BigInteger[] Encrypt(BigInteger m, PublicKey key){
-        BigInteger q = (key.getP().subtract(BigInteger.ONE)).divide(BigInteger.TWO);
-        BigInteger k;
-        do{
-            k = new BigInteger(q.bitLength(), new Random());
-        }while (k.compareTo(q)>=0);
-        BigInteger c1 = key.getG().modPow(k,key.getP());
-        BigInteger c2 = key.getH().modPow()
+    private static BigInteger tirageX(BigInteger p) {
+        boolean found = false;
+        BigInteger pPrime = p.subtract(BigInteger.ONE).divide(BigInteger.TWO);
+        BigInteger x = new BigInteger(pPrime.bitLength(), new Random());
+        while (!found) {
+            if (x.compareTo(pPrime) < 0 && x.compareTo(BigInteger.ZERO) >= 0) {
+                found = true;
+            } else {
+                x = new BigInteger(pPrime.bitLength(), new Random());
+            }
+        }
+
+        return x;
+    }
+    private static BigInteger[] produireC(BigInteger g, BigInteger r, BigInteger p, BigInteger h, int m) {
+
+        BigInteger c[] = {BigInteger.ZERO, BigInteger.ZERO};
+        c[0] = g.modPow(r, p);
+        BigInteger pt1 = g.modPow(BigInteger.valueOf(m), p);
+        BigInteger pt2 = h.modPow(r, p);
+        c[1] = pt1.multiply(pt2);
+
+        return c;
+    }
+
+    private static BigInteger tirageR(BigInteger p) {
+        return tirageX(p);
+    }
+    public static BigInteger[] Encrypt(PublicKey pk, int m) {
+        BigInteger r = tirageR(pk.getP());
+        return produireC(pk.getG(), r, pk.getP(), pk.getH(), m);
+    }
+
+    public static BigInteger Decrypt(BigInteger[] c,  PublicKey key){
+        ArrayList<BigInteger> deltas = new ArrayList<>();
+        int n=holders.size();
+
+        for(int i=0; i<n; i++){
+            int k=0;
+            BigInteger delta = BigInteger.ONE;
+            do{
+                if(k!=i){
+                    delta = delta.multiply( holders.get(k).getX().divide(holders.get(k).getX().subtract(holders.get(i).getX()))).mod(key.getP());
+                }
+                k++;
+            }while (k<n);
+            deltas.add(delta);
+        }
+
+        BigInteger d= BigInteger.ONE;
+        for(int i=0; i<n;i++){
+            d = d.multiply(holders.get(i).computeD(c[0], key.getP()).modPow(deltas.get(i),key.getP()));
+        }
+        BigInteger m = d.modInverse(key.getP()).multiply(c[1]).mod(key.getP());
+        return m;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(KeyGen(5, 3, BigInteger.valueOf(10), 5)));
+        Key[] keys = KeyGen(5,3,BigInteger.valueOf(10), 5);
+
+        PublicKey pk = (PublicKey) keys[0];
+        System.out.println(pk);
+        BigInteger[] c = Encrypt(pk, 4);
+        //BigInteger m = Decrypt(c, pk);
+        //System.out.println(m);
     }
 }
---
+++ Scrutateur/src/main/java/KeyHolder.java
@@ -0,0 +1,32 @@
+import java.math.BigInteger;
+
+public class KeyHolder {
+    private BigInteger x;
+    private BigInteger s;
+
+    private int t;
+
+    private int id;
+
+
+    public KeyHolder(BigInteger x,  BigInteger s) {
+        this.x = x;
+        this.s = s;
+
+
+    }
+
+    public BigInteger getX() {
+        return x;
+    }
+
+    public BigInteger getS() {
+        return s;
+    }
+
+    public BigInteger computeD(BigInteger c, BigInteger p){
+        return c.modPow(s,p);
+    }
+
+
+}
